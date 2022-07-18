0697a21373f7184cbcce1ae8363a7eda463812ab
CLA-GITHUB <christwealthla@gmail.com>
2022-07-18 02:54:48 +0100

    initial commit

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,6 @@
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "dsjfhfjdhfuwe";
+const ACCESS_TOKEN_SECRET = process.env.
 const REFRESH_TOKEN_SECRET =
-  process.env.REFRESH_TOKEN_SECRET || "adfougdfgjkadufh";
+  process.env.REFRESH_TOKEN_SECRET 
 
 module.exports = {
   ACCESS_TOKEN_SECRET,
